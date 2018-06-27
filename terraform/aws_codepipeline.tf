bb6c84483676b6b1617994c2352e678b60ae05bc
toming <tomingtoming@gmail.com>
2018-06-28 08:08:01 +0900

    in progress

--- terraform/aws_codepipeline.tf
+++ terraform/aws_codepipeline.tf
@@ -66,7 +66,7 @@ resource "aws_codepipeline" "codepipeline" {
       version = "1"
 
       configuration {
-        ProjectName = "test"
+        ProjectName = "${var.stage}-serverless-build-deploy"
       }
     }
   }
@@ -120,4 +120,31 @@ resource "aws_s3_bucket" "artifact_store" {
 
 data "aws_kms_alias" "encryption_key" {
   name = "alias/${var.stage}_encryption_key"
+}
+
+resource "aws_codebuild_project" "codebuild" {
+  name          = "${var.stage}_codebuild"
+  description   = "Serverless deploy (stage: ${var.stage})"
+  build_timeout = "5"
+  service_role  = "${aws_iam_role.codepipeline.arn}"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = "${aws_s3_bucket.artifact_store.bucket}"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/golang:1.10"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
 }
